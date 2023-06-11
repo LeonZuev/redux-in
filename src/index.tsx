b7e0acb3db57b59f5dc9c4c18f542ba3e915da62
leonzuev <loonatune@gmail.com>
2023-06-11 03:25:37 +0300

    Refactor redux-in

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-    <Provider store={}>
+  // <React.StrictMode>  // строгий режим
+    <Provider store={store}>
     <App />
     </Provider>
-  // </React.StrictMode>
+  // </React.StrictMode> // строгий режим
 );
