b961f16fdd083ec88236bf53cc913cc9d9b2a4be
leonzuev <loonatune@gmail.com>
2023-06-11 06:30:12 +0300

    Complete Login,Action,AuthState

---
+++ src/features/auth/types/Action.ts
@@ -0,0 +1,3 @@
+type Action = { type: 'auth/saveLogin'; payload: string };
+
+export default Action; // экспортируем, чтобы использовать в других файлах
---
+++ src/features/auth/types/AuthState.ts
@@ -0,0 +1,4 @@
+export default interface AuthSate {
+  login?: string; // ? - variability
+  password?: string;
+}
