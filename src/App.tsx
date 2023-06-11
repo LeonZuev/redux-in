aaf1db866bc3412f5389125a6ae119381f2835be
leonzuev <loonatune@gmail.com>
2023-06-11 08:09:25 +0300

    Add Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useSelector } from 'react-redux';
 import './App.css';
 import Counter from './features/counter/Counter';
+import { useSelector } from 'react-redux';
 import { RootState } from './store';
+import Login from './features/auth/Login';
 
 function App() {
 const value = useSelector((state: RootState) => state.counter.value)
+const user = useSelector((state: RootState) => state.auth.login)
   return (
     <div className="App">
       <h1>Value [{value}]</h1>
       <Counter />
+      <h2>Hello, {user}</h2>
+      <Login />
     </div>
   );
 }
