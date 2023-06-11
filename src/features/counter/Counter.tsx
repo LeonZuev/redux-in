cf80658fcae65f6a4ba97c45355f995d9369078b
leonzuev <loonatune@gmail.com>
2023-06-11 05:19:52 +0300

    Fix bug, add value

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -4,7 +4,7 @@ import { RootState } from "../../store";
 
 export default function Counter(): JSX.Element {
   const dispatch = useDispatch();
-  const value = useSelector((globalState: RootState) => globalState)
+  const value = useSelector((globalState: RootState) => globalState.counter.value)
   return (
     <div>
       <button
@@ -12,10 +12,10 @@ export default function Counter(): JSX.Element {
         onClick={() => dispatch<Action>({ type: 'counter/minus', payload: 1 })}>
         -1
       </button>{' '}
-
+      {value}
       <button
         type="button"
-        onClick={() => dispatch<Action>({ type: 'counter/minus', payload: 1 })}>
+        onClick={() => dispatch<Action>({ type: 'counter/plus', payload: 1 })}>
         +1
       </button>
     </div>
