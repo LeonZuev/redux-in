e91fc19ee990979134066cd1b2d79a8a81c56c6c
leonzuev <loonatune@gmail.com>
2023-06-11 04:44:05 +0300

    Add cases to reducer

--- src/features/counter/counterReducer.ts
+++ src/features/counter/counterReducer.ts
@@ -8,6 +8,8 @@ const initialState: CounterState = {
 export default function counterReducer(state: CounterState = initialState, action: Action): CounterState {
     switch (action.type) {
         case 'counter/minus' :
-            return {}
+            return {...state, value: state.value - action.payload};
+        case 'counter/plus' :
+            return {...state, value: state.value + action.payload}; // либо онимаем либо прибовляем action в value
     }
 }
