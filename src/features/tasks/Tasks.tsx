6e76030038eff5db13f462f207697d107308e7ac
leonzuev <loonatune@gmail.com>
2023-06-16 20:30:31 +0300

    Finish "Добавить"

--- src/features/tasks/Tasks.tsx
+++ src/features/tasks/Tasks.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { SyntheticEvent, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
-export default function Tasks():JSX.Element {
-   const tasks = useSelector((globalState: RootState) =>globalState.tasks);// достали из глобального состояния
+export default function Tasks(): JSX.Element {
+  const tasks = useSelector((globalState: RootState) => globalState.tasks);// достали из глобального состояния
+  const [task, setTask] = useState<string>('');
+  const dispatch = useDispatch();
+  function addTask(e: SyntheticEvent): void {
+    e.preventDefault();
+    dispatch({ type: 'tasks/add', payload: task });
+  }
   return (
     <>
-    <div>Tasks</div>
-    <ul>
-        {tasks.map((el) => 
+      <div>Tasks</div>
+      <form onSubmit={addTask}>
+        <input
+          type="text"
+          required
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit">Добавить</button>
+      </form>
+
+      <ul>
+        {tasks.map((el) =>
         (
           <li key={el.id}>
             {el.title}
           </li>
-        ))}
-    </ul>
+        ))
+        }
+      </ul>
     </>
   );
 }
