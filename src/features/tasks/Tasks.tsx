d92aad02536c4014a2a7e6573a511139b714551c
leonzuev <loonatune@gmail.com>
2023-06-16 20:59:31 +0300

    Add "Edit" button

--- src/features/tasks/Tasks.tsx
+++ src/features/tasks/Tasks.tsx
@@ -3,13 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
 export default function Tasks(): JSX.Element {
-  const tasks = useSelector((globalState: RootState) => globalState.tasks);// достали из глобального состояния
+  const tasks = useSelector((globalState: RootState) => globalState.tasks);
   const [task, setTask] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
   const dispatch = useDispatch();
   function addTask(e: SyntheticEvent): void {
     e.preventDefault();
     dispatch({ type: 'tasks/add', payload: task });
+    setTask('');
   }
+
   return (
     <>
       <div>Tasks</div>
@@ -20,18 +23,42 @@ export default function Tasks(): JSX.Element {
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
-        <button type="submit">Добавить</button>
+        <button type="submit">Add</button>
       </form>
-
       <ul>
-        {tasks.map((el) =>
-        (
-          <li key={el.id}>
-            {el.title}
-          </li>
-        ))
+        {
+          tasks.map((el) =>
+          (
+            <li key={el.id}>
+              {el.title}
+              <button
+                type="button"
+                onClick={
+                  () => dispatch({ type: 'tasks/delete', payload: el.id })
+                }
+              >
+                Delete
+              </button>
+              <form onSubmit={
+                (event)=>{
+                  event.preventDefault();
+                  dispatch({ type: 'tasks/edit', payload: { id: el.id, title}});
+                  setTitle('');
+                }
+              }>
+                <input
+                  type="text"
+                  required
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <button type="submit">Edete</button>
+              </form>
+            </li>
+          ))
         }
       </ul>
+
     </>
   );
 }
